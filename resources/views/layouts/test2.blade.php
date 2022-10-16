87ac83a49970a0434ffed70ae8eee92a4699aa6a
dotlogics09 <dotlogics1999@gmail.com>
2022-10-16 16:01:02 +0530

    new files

---
+++ resources/views/layouts/test2.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    @include('layouts.head')
+</head>
+
+<body>
+    @include('layouts.header')
+
+    @yield('content')
+
+    @include('layouts.copyright')
+
+</body>
+
+</html>
